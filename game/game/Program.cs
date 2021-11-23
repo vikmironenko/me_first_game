64bc8d41b80edac2442daa44146711627d021935
yura159 <yuriy.ivanov.01@bk.ru>
2021-11-23 19:37:54 +0500

    новый комит

--- game/game/Program.cs
+++ game/game/Program.cs
@@ -14,6 +14,7 @@ namespace game
         [STAThread]
         static void Main()
         {
+            /* Новый комент*/
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
